dad0595007776d841e36b3016dcd2939d27d723b
feyagarett <alenushka_11_2001@mail.ru>
2023-05-14 12:26:12 +0300

    addNavigation

--- app/src/main/java/com/shpagat/prosto/MainActivity.kt
+++ app/src/main/java/com/shpagat/prosto/MainActivity.kt
@@ -2,10 +2,23 @@ package com.shpagat.prosto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.shpagat.prosto.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initFields()
+        initFuns()
+    }
+
+    private fun initFields() {
+
+    }
+
+    private fun initFuns() {
+
     }
 }
