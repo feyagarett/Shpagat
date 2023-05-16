54d428b9e5913eb38056a4b4aa2726f4a4d1c337
feyagarett <alenushka_11_2001@mail.ru>
2023-05-16 22:18:57 +0300

    doneAddTrainings

--- app/src/main/java/com/shpagat/prosto/screens/AdminAddTrainingFragment.kt
+++ app/src/main/java/com/shpagat/prosto/screens/AdminAddTrainingFragment.kt
@@ -1,13 +1,13 @@
 package com.shpagat.prosto.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.shpagat.prosto.R
+import androidx.fragment.app.Fragment
 import com.shpagat.prosto.databinding.FragmentAdminAddTrainingBinding
-import com.shpagat.prosto.databinding.FragmentAdminScheduleBinding
+import com.shpagat.prosto.utils.*
+import java.text.SimpleDateFormat
 
 class AdminAddTrainingFragment : Fragment() {
     private lateinit var binding: FragmentAdminAddTrainingBinding
@@ -22,5 +22,46 @@ class AdminAddTrainingFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initFuns()
+    }
+
+    private fun initFuns() {
+        binding.addBtn.setOnClickListener {
+            tryAddTraining()
+        }
+    }
+
+    private fun tryAddTraining() {
+        val title = myGetText(binding.title)
+        val coach = myGetText(binding.coach)
+        val price = myGetText(binding.price)
+        val places = myGetText(binding.places)
+        val date = myGetText(binding.date)
+        if (title.isNotEmpty() && coach.isNotEmpty() && price.isNotEmpty()
+            && places.isNotEmpty() && date.isNotEmpty()
+        ) {
+            try {
+                val longDate = SimpleDateFormat("dd.MM.yy HH:mm").parse(date).time
+                val trainingDb = database.child(TRAININGS).child(longDate.toString())
+                trainingDb.child(TITLE).setValue(title)
+                trainingDb.child(COACH).setValue(coach)
+                trainingDb.child(PRICE).setValue(price)
+                trainingDb.child(PLACES).setValue(places)
+                appToast("Успех")
+                clearInputs()
+            } catch (e: Exception) {
+                appToast("Неверный формат даты и времени")
+            }
+        } else {
+            appToast("Заполните поля")
+        }
+    }
+
+    private fun clearInputs() {
+        binding.title.text.clear()
+        binding.coach.text.clear()
+        binding.price.text.clear()
+        binding.date.text.clear()
+        binding.places.text.clear()
     }
 }
--- app/src/main/java/com/shpagat/prosto/screens/AdminScheduleFragment.kt
+++ app/src/main/java/com/shpagat/prosto/screens/AdminScheduleFragment.kt
@@ -1,16 +1,21 @@
 package com.shpagat.prosto.screens
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.recyclerview.widget.RecyclerView
 import com.shpagat.prosto.R
-import com.shpagat.prosto.databinding.FragmentAdminBinding
+import com.shpagat.prosto.adapter.TrainingsAdapter
 import com.shpagat.prosto.databinding.FragmentAdminScheduleBinding
+import com.shpagat.prosto.utils.APP
 
 class AdminScheduleFragment : Fragment() {
     private lateinit var binding: FragmentAdminScheduleBinding
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: TrainingsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +27,18 @@ class AdminScheduleFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        initList()
+        initFuns()
+    }
+
+    private fun initFuns() {
+        binding.addBtn.setOnClickListener {
+            val navController = Navigation.findNavController(APP, R.id.main_frame)
+            navController.navigate(R.id.action_adminSchedule_to_adminAddTraining)
+        }
+    }
+
+    private fun initList() {
+
     }
 }
